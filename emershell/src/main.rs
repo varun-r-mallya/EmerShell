6e9b623286122cf1fadb85ebf44f1e3f54ce4e54
varun-r-mallya <varunrmallya@gmail.com>
2024-06-13 11:03:36 +0000

    hehe boi. almost added IP addr detection

--- emershell/src/main.rs
+++ emershell/src/main.rs
@@ -2,12 +2,23 @@ use core::panic;
 use std::{
     fs::{self},
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream,},             //TODO: add this functionality if required  IpAddr, Ipv4Addr
+    process::Command,
+
 };
+use dotenv::dotenv;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:1024").unwrap();
+    dotenv().ok();
+    let port_number = std::env::var("PORT").expect("PORT number must be set.");
+    //let _username = std::env::var("USERNAME").expect("USERNAME must be set.");                  //TODO: this has the password (remove the "_")
+    //let _password = std::env::var("PASSWORD").expect("USERNAME must be set.");                  //TODO: this has the username
 
+    let ipaddr: String = local_ip_detect();
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{port_number}")).unwrap();
+    
+    println!("Server running on http://{ipaddr}:{port_number}");
     for stream in listener.incoming() {
         let stream = match stream {
             Ok(_) => {stream.unwrap()},
@@ -26,7 +37,6 @@ fn handle_connection(mut stream: TcpStream){
         Some(request_content) => {
             match request_content {
                 Ok(s) => {
-                        println!("{}", s);
                         route_decider(s, stream);
                 },
                 Err(er) => {
@@ -45,6 +55,13 @@ fn route_decider(request_line: String, mut stream: TcpStream) {
     } else 
     if request_line == "POST /login HTTP/1.1" {
         authoriser(stream);                                     //login API
+        return;
+    } else 
+    if request_line == "GET /ping HTTP/1.1" {
+        match stream.write_all("pong 🏓".as_bytes()) {
+            Ok(s) => s,
+            Err(er) => println!("Error in writing to the stream: {}", er)
+        };
         return;
     } else {
         ("HTTP/1.1 404 NOT FOUND", "views/404.html")            //error page
@@ -67,4 +84,38 @@ fn page_display(filename: &str, status_line: &str) -> String {
 
 fn authoriser(mut stream: TcpStream) {
     println!("authorise function ran")
+
 }
+
+fn local_ip_detect() -> String {
+    let os = std::env::var("OS").expect("Operating system env variable (OS) must be set.");
+    let output = 
+    if cfg!(target_os = "linux") && os == "ARCH" 
+    {
+        Command::new("sh")
+            .arg("-c")
+            .arg("ip addr")
+            .output()
+            .expect("failed to execute process")
+    } else 
+    if cfg!(target_os = "linux") && os == "UBUNTU" 
+    {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo helloi")
+            .output()
+            .expect("failed to execute process")
+    }
+    else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo either use ubuntu or arch. Also, if you are aren't on linux yet, wtf are you doing brother.")
+            .output()
+            .expect("failed to execute process")
+    };
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    println!("{}", stdout);
+    "balls".to_string()
+    
+}
+    
